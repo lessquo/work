9945473b1097e04514354b7643abbeeec5192d4b
Seong-Yun Min <97732161+lessquo@users.noreply.github.com>
2026-05-02 18:52:34 +0900

    Drop RootTabs header

--- src/routes/RootLayout.tsx
+++ src/routes/RootLayout.tsx
@@ -1,15 +1,8 @@
-import { RootTabs } from '@/components/RootTabs';
-import { Link, Outlet } from 'react-router';
+import { Outlet } from 'react-router';
 
 export function RootLayout() {
   return (
     <div className='flex h-dvh flex-col'>
-      <header className='h-header flex items-center gap-2 border-b bg-white px-4'>
-        <Link to='/' className='font-semibold tracking-tight'>
-          Work
-        </Link>
-        <RootTabs />
-      </header>
       <Outlet />
     </div>
   );
